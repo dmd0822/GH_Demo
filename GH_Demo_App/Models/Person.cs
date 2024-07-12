5007eacd0f39ebbe65d3fb942d67e20dbee6275e
Dave Davis <dave@davemdavis.net>
2024-07-12 10:03:03 -0400

    Add Person class to GH_Demo_App.Models namespace
    A new `Person` class has been introduced within the `GH_Demo_App.Models` namespace. This class contains three properties: `FirstName`, `LastName`, and `DateOfBirth`, which are used to store a person's first name, last name, and date of birth, respectively.
    
    #1

---
+++ GH_Demo_App/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace GH_Demo_App.Models
+{
+    //Create a person class with first name last name and date of birth
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+   
+}
